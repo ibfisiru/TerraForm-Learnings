d3955a41ce2ce65955a4144876c17db7dbea419c
Ibrahim F <ibufiziru@gmail.com>
2025-09-07 16:02:17 -0400

    Update main.tf
    Changed the default instance OS and type

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+resource "aws_instance" "Tester" {
+  ami = "ami-0cfde0ea8edd312d4" # Ubuntu Server 24.04 LTS (HVM),EBS General Purpose (SSD) Volume Type. Support available from Canonical // us-east-1
+  instance_type = "t3.micro"
 }
