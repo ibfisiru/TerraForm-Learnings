70a0a71c4ab634888a53eab6ee81b0638dc31e1c
Ibrahim F <ibufiziru@gmail.com>
2025-10-11 14:14:41 -0400

    Update compute.tf

--- 06-organization-and-modules/web-app-module/compute.tf
+++ 06-organization-and-modules/web-app-module/compute.tf
@@ -1,21 +1,28 @@
-resource "aws_instance" "instance_1" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 1" > index.html
-              python3 -m http.server 8080 &
-              EOF
-}
-
-resource "aws_instance" "instance_2" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 2" > index.html
-              python3 -m http.server 8080 &
-              EOF
+		resource "aws_instance" "webAppInstance1" {
+		          tags = {
+		          Name = "var.instance1_name"
+		                        }
+			ami           = var.ami 
+			instance_type = var.instance_type
+			security_groups = [aws_security_group.instances.name]
+			user_data       = <<-EOF
+				#!/bin/bash
+				echo "Hello, World 1" > index.html
+				python3 -m http.server 8080 &
+				EOF
+					}
+					
+					
+		resource "aws_instance" "webAppInstance2" {
+		            tags = {
+		             name = "var.instance2_name"
+		                             }
+			ami           = var.ami
+			instance_type = var.instance_type
+			security_groups = [aws_security_group.instances.name]
+			user_data       = <<-EOF
+				#!/bin/bash
+				echo "Hello, World 2" > index.html
+				python3 -m http.server 8080 &
+				 EOF
 }
