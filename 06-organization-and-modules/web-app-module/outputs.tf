66d028fd7d8edc2654aadba720a6623cb0caf7ee
Ibrahim F <ibufiziru@gmail.com>
2025-10-11 14:17:50 -0400

    Update outputs.tf

--- 06-organization-and-modules/web-app-module/outputs.tf
+++ 06-organization-and-modules/web-app-module/outputs.tf
@@ -1,11 +1,13 @@
-output "instance_1_ip_addr" {
-  value = aws_instance.instance_1.public_ip
-}
-
-output "instance_2_ip_addr" {
-  value = aws_instance.instance_2.public_ip
-}
-
-output "db_instance_addr" {
-  value = aws_db_instance.db_instance.address
+		# To Display the IP Of The 2 EC2 Instances & The RDS Instance
+		
+				output "instance_1_ip_addr" {
+				  value = aws_instance.webAppInstance1.public_ip
+				}
+				
+				output "instance_2_ip_addr" {
+				  value = aws_instance.webAppInstance2.public_ip
+				}
+				
+				output "db_instance_addr" {
+				  value = aws_db_instance.db_instance.address
 }
